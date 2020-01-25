725cba1df44796c5943e5dca679234fcb304bc72
Chris Tessum <ctessum@gmail.com>
2020-01-24 19:41:10 -0600

    Fully replace training and validation loop with keras.fit()

--- training.py
+++ training.py
@@ -7,15 +7,13 @@ import tensorflow as tf
 import helperfns
 import networkarch as net
 
-#tf.compat.v1.disable_eager_execution()
 tf.keras.backend.set_floatx('float64')
 
 
-def try_net(data_val, params):
+def try_net(params):
     """Run a random experiment for particular params and data.
 
     Arguments:
-        data_val -- array containing validation dataset
         params -- dictionary of parameters for experiment
 
     Returns:
@@ -24,7 +22,6 @@ def try_net(data_val, params):
     Side effects:
         Changes params dict
         Saves files
-        Builds TensorFlow graph (reset in main_exp)
     """
     # SET UP NETWORK
     deep_koop = net.DeepKoopman(params)
@@ -50,127 +47,34 @@ def try_net(data_val, params):
         run_eagerly=True,
     )
 
-    data_train = np.loadtxt(('./data/%s_train%d_x.csv' % (params['data_name'], 1)), delimiter=',',
-                    dtype=np.float64)
-    data_train_tensor = helperfns.stack_data(data_train, max_shifts_to_stack, params['len_time'])
+    # Load training data
+    for f in range(params['data_train_len']):
+        file_num = f + 1  # 1...data_train_len
+        data_train = np.loadtxt(('./data/%s_train%d_x.csv' % (params['data_name'], file_num)), delimiter=',',
+                                    dtype=np.float64)
+        data_train_tensor_temp = helperfns.stack_data(data_train, max_shifts_to_stack, params['len_time'])
+        if f==0: data_train_tensor = data_train_tensor_temp
+        else: data_train_tensor = tf.concat([data_train_tensor, data_train_tensor_temp], axis=1)
     data_train_tensor = tf.transpose(data_train_tensor, perm=[1, 0, 2])
 
+    # Load validation data
+    # data is num_steps x num_examples x n but load flattened version (matrix instead of tensor)
+    data_val = np.loadtxt(('./data/%s_val_x.csv' % (params['data_name'])), delimiter=',', dtype=np.float64)
     data_val_tensor = helperfns.stack_data(data_val, max_shifts_to_stack, params['len_time'])
+    data_val_tensor = tf.transpose(data_val_tensor, perm=[1, 0, 2])
+
+    reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', patience=10)
+    stop_early = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=15, restore_best_weights=True)
 
     deep_koop.fit(
         x=data_train_tensor,
         y=data_train_tensor,
-        epochs=50,
+        validation_data=(data_val_tensor, data_val_tensor),
+        shuffle=True,
+        epochs=params['num_passes_per_file'],
         batch_size=params["batch_size"],
+        callbacks=[reduce_lr, stop_early],
     )
-
-    # LAUNCH GRAPH AND INITIALIZE
-    sess = tf.compat.v1.Session()
-    saver = tf.compat.v1.train.Saver()
-
-    # Before starting, initialize the variables.  We will 'run' this first.
-    init = tf.compat.v1.global_variables_initializer()
-    sess.run(init)
-
-    csv_path = params['model_path'].replace('model', 'error')
-    csv_path = csv_path.replace('ckpt', 'csv')
-    print(csv_path)
-
-    num_saved_per_file_pass = params['num_steps_per_file_pass'] / 20 + 1
-    num_saved = np.floor(num_saved_per_file_pass * params['data_train_len'] * params['num_passes_per_file']).astype(int)
-    train_val_error = np.zeros([num_saved, 16])
-    count = 0
-    best_error = 10000
-
-    data_val_tensor = helperfns.stack_data(data_val, max_shifts_to_stack, params['len_time'])
-
-    start = time.time()
-    finished = 0
-    saver.save(sess, params['model_path'])
-
-    # TRAINING
-    # loop over training data files
-    for f in range(params['data_train_len'] * params['num_passes_per_file']):
-        if finished:
-            break
-        file_num = (f % params['data_train_len']) + 1  # 1...data_train_len
-
-        if (params['data_train_len'] > 1) or (f == 0):
-            # don't keep reloading data if always same
-            data_train = np.loadtxt(('./data/%s_train%d_x.csv' % (params['data_name'], file_num)), delimiter=',',
-                                    dtype=np.float64)
-            data_train_tensor = helperfns.stack_data(data_train, max_shifts_to_stack, params['len_time'])
-            num_examples = data_train_tensor.shape[1]
-            num_batches = int(np.floor(num_examples / params['batch_size']))
-
-        ind = np.arange(num_examples)
-        np.random.shuffle(ind)
-        data_train_tensor = data_train_tensor[:, ind, :]
-
-        # loop over batches in this file
-        for step in range(params['num_steps_per_batch'] * num_batches):
-
-            if params['batch_size'] < data_train_tensor.shape[1]:
-                offset = (step * params['batch_size']) % (num_examples - params['batch_size'])
-            else:
-                offset = 0
-
-            batch_data_train = data_train_tensor[:, offset:(offset + params['batch_size']), :]
-
-            feed_dict_train = {x: batch_data_train}
-            feed_dict_train_loss = {x: batch_data_train}
-            feed_dict_val = {x: data_val_tensor}
-
-            if (not params['been5min']) and params['auto_first']:
-                sess.run(optimizer_autoencoder, feed_dict=feed_dict_train)
-            else:
-                sess.run(optimizer, feed_dict=feed_dict_train)
-
-            if step % 20 == 0:
-                train_error = sess.run(loss, feed_dict=feed_dict_train_loss)
-                val_error = sess.run(loss, feed_dict=feed_dict_val)
-                train_error, val_error = train_error.mean(), val_error.mean()
-
-                if val_error < (best_error - best_error * (10 ** (-5))):
-                    best_error = val_error.copy()
-                    saver.save(sess, params['model_path'])
-                    print("New best val error %f" % (best_error))
-
-                train_val_error[count, 0] = train_error
-                train_val_error[count, 1] = val_error
-                train_val_error[count, 4] = sess.run(loss1, feed_dict=feed_dict_train_loss).mean()
-                train_val_error[count, 5] = sess.run(loss1, feed_dict=feed_dict_val).mean()
-                train_val_error[count, 6] = sess.run(loss2, feed_dict=feed_dict_train_loss).mean()
-                train_val_error[count, 7] = sess.run(loss2, feed_dict=feed_dict_val).mean()
-                train_val_error[count, 8] = sess.run(loss3, feed_dict=feed_dict_train_loss).mean()
-                train_val_error[count, 9] = sess.run(loss3, feed_dict=feed_dict_val).mean()
-                train_val_error[count, 10] = sess.run(loss_Linf, feed_dict=feed_dict_train_loss).mean()
-                train_val_error[count, 11] = sess.run(loss_Linf, feed_dict=feed_dict_val).mean()
-                if np.isnan(train_val_error[count, 10]):
-                    params['stop_condition'] = 'loss_Linf is nan'
-                    finished = 1
-                    break
-
-                np.savetxt(csv_path, train_val_error, delimiter=',')
-                finished, save_now = helperfns.check_progress(start, best_error, params)
-                count = count + 1
-                if save_now:
-                    train_val_error_trunc = train_val_error[range(count), :]
-                    helperfns.save_files(sess, csv_path, train_val_error_trunc, params, weights, biases)
-                if finished:
-                    break
-
-            if step > params['num_steps_per_file_pass']:
-                params['stop_condition'] = 'reached num_steps_per_file_pass'
-                break
-
-    # SAVE RESULTS
-    train_val_error = train_val_error[range(count), :]
-    print(train_val_error)
-    params['time_exp'] = time.time() - start
-    saver.restore(sess, params['model_path'])
-    helperfns.save_files(sess, csv_path, train_val_error, params, weights, biases)
-    tf.compat.v1.reset_default_graph()
 
 
 def main_exp(params):
@@ -194,6 +98,4 @@ def main_exp(params):
 
     tf.compat.v1.set_random_seed(params['seed'])
     np.random.seed(params['seed'])
-    # data is num_steps x num_examples x n but load flattened version (matrix instead of tensor)
-    data_val = np.loadtxt(('./data/%s_val_x.csv' % (params['data_name'])), delimiter=',', dtype=np.float64)
-    try_net(data_val, params)
+    try_net(params)
